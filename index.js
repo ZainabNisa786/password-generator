03c7c29c478a5162b7dfddf19aad1ffd21a8234d
Zainab Nisa <nisazainab53@gmail.com>
2023-02-24 20:18:35 +0500

    js project

---
+++ index.js
@@ -0,0 +1,24 @@
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+"S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+"p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+"~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",",
+"|",":",";","<",">",".","?","/"];
+
+
+function genPass(){
+    let passOne = document.getElementById("btn-one")
+    let passTwo = document.getElementById("btn-two")
+    passLength=12
+    let genOne = ""
+    let genTwo = ""
+    for(let i=0; i < passLength; i++){
+    let indexOne = characters[Math.floor(Math.random()*characters.length)]
+    let indexTwo = characters[Math.floor(Math.random()*characters.length)]
+    genOne+=indexOne
+    genTwo+=indexTwo
+    passOne.textContent=genOne
+    passTwo.textContent=genTwo
+    }
+    return genPass
+
+}
